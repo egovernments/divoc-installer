f214ec755fb8c0c197d552dd8993284bce0858fe
prasanna <prasanna@beehyv.com>
2022-02-02 16:09:12 +0530

    setup Kafka Zookeeper

--- install.sh
+++ install.sh
@@ -35,12 +35,12 @@ installDependencies()
     fi
 #     echo "Enter path to inventory file: "
 #     read -r INVENTORY_FILE
-#    ansible-playbook -vvv -i ./inventory ./ansible-cookbooks/docker-registry/playbook.yml
-#    ansible-playbook -vvv -i ./inventory ./ansible-cookbooks/elastic-search/playbook.yml
-#    ansible-playbook -vvv -i ./inventory ./ansible-cookbooks/redis/playbook.yml
-#    ansible-playbook -vvv -i ./inventory ./ansible-cookbooks/kafka-zookeeper/playbook.yml
-#    ansible-playbook -vvv -i ./inventory ./ansible-cookbooks/kubernetes/playbook.yml
-    ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook  -i ./inventory ./ansible-cookbooks/postgres-etcd/deploy_pgcluster.yml
+#    ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ./inventory ./ansible-cookbooks/docker-registry/playbook.yml
+#    ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ./inventory ./ansible-cookbooks/elastic-search/playbook.yml
+#    ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ./inventory ./ansible-cookbooks/redis/playbook.yml
+    ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ./inventory ./ansible-cookbooks/kafka-zookeeper/playbook.yml
+#    ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ./inventory ./ansible-cookbooks/kubernetes/playbook.yml
+#    ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook  -i ./inventory ./ansible-cookbooks/postgres-etcd/deploy_pgcluster.yml
 }
 
 installDependencies
